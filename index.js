8e389fc9f2a8fe36aff0c268760fa66f5d727a90
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-05 07:38:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ function displayRepositories() {
           <li>
             <h2>${repo.name}</h2>
             <a href="${repo.html_url}">${repo.html_url}</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+            </li>
           </li>`
           )
   }).join('') + "</ul>";
