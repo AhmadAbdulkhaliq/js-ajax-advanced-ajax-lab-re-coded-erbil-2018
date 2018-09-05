3ad44b0359a9e66bf2386f9dcead4ba25d093553
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-05 07:36:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const rootURL = "https://api.github.com"
 
 function getRepositories() {
-  const name = document.getElementById("username").value
-  const uri = rootURL + "/users/" + name + "/repos"
+  const uri = rootURL + "/users/AhmadAbdulkhaliq/repos"
   const xhr = new XMLHttpRequest()
   xhr.addEventListener("load", displayRepositories)
   xhr.open("GET", uri)
