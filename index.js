5ab6c48527a9035e102167c1a2bb8a9c692a548a
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-05 07:34:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
-function getRepos() {
-  const req = new XMLHttpRequest()
-  req.addEventListener("load", showRepositories);
-  req.open("GET", 'https://api.github.com/users/A7madXatab/repos')
-  req.send()
+const rootURL = "https://api.github.com"
+
+function getRepositories() {
+  const name = document.getElementById("username").value
+  const uri = rootURL + "/users/" + name + "/repos"
+  const xhr = new XMLHttpRequest()
+  xhr.addEventListener("load", displayRepositories)
+  xhr.open("GET", uri)
+  xhr.send()
+  return false;
 }
-function showRepositories(event,data)
-{
-  let repos = JSON.parse(this.responseText);
-  let reposList = document.createElement("ul");
-  for(let i=0;i<repos.length;i++)
-   {
-     let li = document.createElement("li");
-    let a = document.createElement("a");
-  a.setAttribute("href",repos[i]["html_url"]);
-  a.innerHTML = "Go to ";
-  let h2 = document.createElement("h4");
-  h2.appendChild(a);
-  h2.innerHTML = h2.innerHTML+ repos[i]["name"];
-  li.appendChild(h2);
-  
-  let watchersP = document.createElement("p");
-  watchersP.innerHTML = "WatchersCount "+repos[i]["watchers_count"];
-  let forksP = document.createElement("p");
-  forksP.innerHTML = "Forks count "+repos[i]["forks_count"];
-  let isuuesP = document.createElement("p");
-  isuuesP.innerHTML ="isuues Count "+repos[i]["open_issues_count"];
-  li.appendChild(watchersP);
-  li.appendChild(forksP);
-  li.appendChild(isuuesP);
-  
-  reposList.appendChild(li);
-   }
-   console.log(reposList)
-   document.getElementById("repositories").appendChild(reposList);
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText)
+  const repoList = "<ul>" + repos.map(repo => {
+    const dataUsername = 'data-username="' + repo.owner.login + '"'
+    const dataRepoName = 'data-repository="' + repo.name + '"'
+    return(`
+          <li>
+            <h2>${repo.name}</h2>
+            <a href="${repo.html_url}">${repo.html_url}</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+          </li>`
+          )
+  }).join('') + "</ul>";
+  document.getElementById("repositories").innerHTML = repoList
+}
+function getCommits(el) {
+  const repoName = el.dataset.repository
+  const uri = rootURL + "/repos/" + el.dataset.username + "/" + repoName + "/commits"
+  const xhr = new XMLHttpRequest()
+  xhr.addEventListener("load", displayCommits)
+  xhr.open("GET", uri)
+  xhr.send()
+}
+function displayCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name + ' (' + commit.author + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = commitsList
+}
+function getBranches(el) {
+  const repoName = el.dataset.repository
+  const uri = rootURL + "/repos/" + el.dataset.username + "/" + repoName + "/branches"
+  const xhr = new XMLHttpRequest()
+  xhr.addEventListener("load", displayBranches)
+  xhr.open("GET", uri)
+  xhr.send()
+}
+function displayBranches() {
+  const branches = JSON.parse(this.responseText)
+  const branchesList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = branchesList
 }
